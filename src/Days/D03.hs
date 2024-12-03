d2b53fa423ea746135046185362d704899f8c73e
three genders in a wenchcoat <tanuki@gmail.com>
2024-12-03 01:10:13 -0800

    puzzle 3b

--- src/Days/D03.hs
+++ src/Days/D03.hs
@@ -1,17 +1,20 @@
 {-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Days.D03 where
 
 import Lib
+import Data.Text qualified as Text
 import Text.Regex.TDFA ( (=~), AllTextMatches(getAllTextMatches) )
 import Data.List qualified as List
 import Data.Char (isNumber)
 
 dispatch :: Dispatch
-dispatch = dispatchWith parseInput part1 undefined
+dispatch = dispatchWith id part1 part2
 --
 
-part1 :: String -> Int
+part1, part2 :: String -> Int
 part1 = sum . fmap exec . parseInput
+part2 = part1 . thereIsNoTry
 
 data Expr
   = Val Int
@@ -28,6 +31,15 @@ parseInput = fmap parseMul . extractMuls
   matchMul :: String
   matchMul = "mul\\([0-9]{1,3},[0-9]{1,3}\\)"
   extractMuls input = getAllTextMatches (input =~ matchMul)
+
+thereIsNoTry :: String -> String
+thereIsNoTry input = onlyDos
+  where
+  Just (beforeFirstDont, dontGroups)
+    = List.uncons . Text.splitOn "don't()" $ Text.pack input
+  doAfterDont = snd . Text.breakOn "do()" <$> dontGroups
+  doGroups = beforeFirstDont : doAfterDont
+  onlyDos = Text.unpack $ Text.concat doGroups
 
 parseMul :: String -> Expr
 parseMul mulStr = Mul (Val x) (Val y)
