f7101bf578b038549a8277bf51795a83f3fdd985
three genders in a wenchcoat <tanuki@gmail.com>
2024-12-04 03:53:22 -0800

    better future-days template

--- src/Days/D06.hs
+++ src/Days/D06.hs
@@ -1,7 +1,12 @@
 module Days.D06 where
 
-import Lib
+import Lib ( Dispatch, dispatchWith )
 
 dispatch :: Dispatch
-dispatch = undefined
---
+dispatch = dispatchWith part1 part2
+
+part1 :: String -> Int
+part1 = undefined
+
+part2 :: String -> Int
+part2 = undefined
