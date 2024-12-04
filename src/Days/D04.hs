509e8cc4e044c28a7c7638fb2492c4bef592200e
David Loring <david@dthl.me>
2024-12-04 01:39:47 -0800

    Wrote a permutation generator for search. Rejected generating all six directions upfront because I'm trying not to be too crazy.

--- src/Days/D04.hs
+++ src/Days/D04.hs
@@ -1,7 +1,37 @@
 module Days.D04 where
 
-import Lib
-
+import Lib ( dispatchWith, Dispatch )
 dispatch :: Dispatch
-dispatch = undefined
+dispatch = dispatchWith words part1 part2
 --
+part1, part2 :: String -> Int
+part1 = undefined
+part2 = undefined
+
+getFromLists :: (Int,Int) -> [String] -> [Char]
+getFromLists (x,y) s
+    | (x < 0) || (y < 0) = ""
+    | (x > (length s - 1)) = ""
+    | (y > length (s !! 0) - 1) = ""
+    | otherwise = s !! x !! y : []
+
+eightWaySearch :: (Int,Int) -> Int -> [String] -> [String]
+eightWaySearch (x,y) 0 s = replicate 9 (getFromLists (x,y) s)
+eightWaySearch (x,y) l s = zipWith (<>) (eightWaySearch(x,y) (l-1) s) (fmap (flip getFromLists s) coords) 
+    where 
+        coords = [(x + l * a, y + l * b) | a <- [-1..1], b <- [-1..1]]
+
+
+-- 
+
+-- parseInput :: String -> [String]
+-- parseInput l = reverseLists l <> diagonalLists l <> verticalLists l <> words l
+
+-- reverseLists :: String -> [String]
+-- reverseLists l = fmap reverse verticalLists l <> diagonalLists l <> lines l
+
+-- verticalLists :: String -> [String]
+-- verticalLists = transpose . words 
+
+-- diagonalLists :: String -> [String]
+-- diagonalLists = undefined 
