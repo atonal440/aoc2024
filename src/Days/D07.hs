6ee0af44b9c75a820792ca3c69f5de590e2772f2
three genders in a wenchcoat <tanuki@gmail.com>
2024-12-16 20:35:52 -0800

    puzzle 7a

--- src/Days/D07.hs
+++ src/Days/D07.hs
@@ -1,12 +1,40 @@
 module Days.D07 where
 
 import Lib ( Dispatch, dispatchWith )
+import Data.Bifunctor ( bimap )
+import Control.Monad ( guard )
+import Debug.Trace (traceShowId)
+import Data.List qualified as List
 
 dispatch :: Dispatch
 dispatch = dispatchWith part1 part2
 
 part1 :: String -> Int
-part1 = undefined
+part1 = sum . traceShowId . run1 . parseInput
 
 part2 :: String -> Int
 part2 = undefined
+
+data TestSet = TestSet
+  { value    :: Int
+  , operands :: [Int]
+  } deriving Show
+
+run1 :: [TestSet] -> [Int]
+run1 input = do
+  testSet <- input
+  check <- List.nub $ permute testSet.operands
+  guard $ check == testSet.value
+  pure testSet.value
+  where
+  permute :: [Int] -> [Int]
+  permute (x:y:zs) = do
+    w <- [op x y | op <- [(+), (*)]]
+    permute $ w : zs
+  permute xs = xs
+
+parseInput :: String -> [TestSet]
+parseInput = fmap parseLine . lines
+  where
+  parseLine = uncurry TestSet . bimap read parseOperands . break (==':')
+  parseOperands = fmap read . words . drop 1
