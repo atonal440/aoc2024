2f026c79656959a9582ae48dc06490daeed11675
three genders in a wenchcoat <tanuki@gmail.com>
2024-12-19 03:28:40 -0800

    puzzle 9b

--- src/Days/D09.hs
+++ src/Days/D09.hs
@@ -3,7 +3,12 @@ module Days.D09 where
 
 import Lib ( Dispatch, dispatchWith )
 import Data.List qualified as List
-import Data.Maybe (catMaybes)
+import Data.Maybe ( catMaybes, listToMaybe, fromJust )
+import Data.Vector ( Vector )
+import Data.Vector qualified as Vec
+import Control.Arrow ( (&&&) )
+import Data.Map ( Map )
+import Data.Map qualified as Map
 
 dispatch :: Dispatch
 dispatch = dispatchWith part1 part2
@@ -12,7 +17,7 @@ part1 :: String -> Int
 part1 = checksum . compact . parseInput
 
 part2 :: String -> Int
-part2 = undefined
+part2 = checksum . defrag . parseInput
 
 type Sector = Maybe Int
 
@@ -36,6 +41,42 @@ compact sectors
     , file : compacted
     )
 
+defrag :: [Sector] -> [Sector]
+defrag sectors = Vec.toList $ defrag' segments sectorVector
+  where
+  sectorVector = Vec.fromList sectors
+  segments = Map.fromList . fmap mapData . List.group $ catMaybes sectors
+  mapData segment = let fileIndex = List.head segment in
+    ( fileIndex
+    , ( fromJust $ Vec.elemIndex (Just fileIndex) sectorVector
+      , length segment
+    ) )
+
+defrag' :: Map Int (Int,Int) -> Vector Sector -> Vector Sector
+defrag' segments sectors
+  | null segments = sectors
+  | otherwise = defrag' segments' sectors'
+  where
+  ((fileId, (fileIx, fileSz)), segments') = Map.deleteFindMax segments
+  earliestValidTarget
+    = listToMaybe . fmap fst
+    . filter ( (>= fileSz) . snd )
+    . filter ( (< fileIx) . fst )
+    . fmap (List.head &&& List.length)
+    . Vec.foldr groupSequential []
+    $ Vec.elemIndices Nothing sectors
+  sectors' = case earliestValidTarget of
+    Nothing -> sectors
+    Just target -> sectors Vec.//
+      ( mkUpd target fileSz (Just fileId) <> mkUpd fileIx fileSz Nothing )
+  mkUpd ix sz val = zip [ix..ix+sz-1] $ repeat val
+  groupSequential :: Int -> [[Int]] -> [[Int]]
+  groupSequential x [] = [[x]]
+  groupSequential x acc@(ys:zs)
+    | x == pred (head ys) = (x:ys):zs
+    | otherwise           = [x]:acc
+
+--
 parseInput :: String -> [Sector]
 parseInput = snd . foldl' interleave ((0, False), []) . fmap (read . pure)
   where
