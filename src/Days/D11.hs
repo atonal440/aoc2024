203a7f7b8fc75bbf4d584d2223a39840385903fe
three genders in a wenchcoat <tanuki@gmail.com>
2024-12-21 00:52:04 -0800

    puzzle 11a

--- src/Days/D11.hs
+++ src/Days/D11.hs
@@ -1,12 +1,29 @@
+{-# OPTIONS_GHC -Wno-x-partial #-}
 module Days.D11 where
 
-import Lib ( Dispatch, dispatchWith )
+import Lib ( Dispatch, dispatchWith, countDigits )
+import Data.Bool ( bool )
 
 dispatch :: Dispatch
 dispatch = dispatchWith part1 part2
 
 part1 :: String -> Int
-part1 = undefined
+part1 = sum . iterateRules 25 . fmap read . words
 
 part2 :: String -> Int
 part2 = undefined
+
+iterateRules :: Int -> [Int] -> [Int]
+iterateRules n = head . drop n . iterate (concatMap rules)
+
+rules :: Int -> [Int]
+rules 0 = [1]
+rules x = bool boost split $ digits `mod` 2 == 0
+  where
+  digits = countDigits x
+  boost = [x * 2024]
+  split = let
+    shift = 10 ^ (digits `div` 2)
+    upper = x `div` shift
+    lower = x - upper * shift
+    in [upper, lower]
